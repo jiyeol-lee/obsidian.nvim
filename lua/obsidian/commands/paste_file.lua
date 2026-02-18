2a4e431ad0f2628000a5c4750c52031b5abb6615
Jiyeol Lee <41712713+jiyeol-lee@users.noreply.github.com>
2026-02-18 12:59:39 -0500

    feat: support TMPDIR for pasted img/file

--- lua/obsidian/commands/paste_file.lua
+++ lua/obsidian/commands/paste_file.lua
@@ -4,9 +4,27 @@ local paste_file = require("obsidian.file_paste").paste_file
 
 ---@param client obsidian.Client
 return function(client, data)
-  local file_folder = Path.new(client.opts.attachments.file_folder)
-  if not file_folder:is_absolute() then
-    file_folder = client.dir / client.opts.attachments.file_folder
+  -- Detect --tmp-dir flag
+  local use_tmp_dir = false
+  local args = data.args
+
+  if args == "--tmp-dir" then
+    use_tmp_dir = true
+    args = nil
+  elseif args ~= nil and vim.endswith(args, " --tmp-dir") then
+    use_tmp_dir = true
+    args = util.strip_whitespace(string.sub(args, 1, -string.len " --tmp-dir" - 1))
+  end
+
+  -- Determine folder based on flag
+  local file_folder
+  if use_tmp_dir then
+    file_folder = util.get_tmp_dir_path(client.opts.attachments.file_folder)
+  else
+    file_folder = Path.new(client.opts.attachments.file_folder)
+    if not file_folder:is_absolute() then
+      file_folder = client.dir / client.opts.attachments.file_folder
+    end
   end
 
   ---@type string|?
@@ -16,7 +34,7 @@ return function(client, data)
   end
 
   local path = paste_file {
-    fname = data.args,
+    fname = args,
     default_dir = file_folder,
     default_name = default_name,
     should_confirm = client.opts.attachments.confirm_file_paste,
